99d378ebd55ccdc1517968a2de27241781e76322
xmile4ever <161158995+xmile4ever@users.noreply.github.com>
2025-04-25 22:14:32 -0700

    Delete docs/test-3/level-2/assets directory

--- docs/test-3/level-2/assets/myjs.js
+++
@@ -1,6 +0,0 @@
-    var button = document.querySelector( "button" );
- 
-    // If a user clicks on it, say hello!
-    button.addEventListener( "click", function( ev ) {
-        alert( "Hello" );
-    }, false)
