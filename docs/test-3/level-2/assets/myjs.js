af098f438200a2fae02108627cf6435bdc698eba
xmile4ever <161158995+xmile4ever@users.noreply.github.com>
2025-04-25 22:04:40 -0700

    Create myjs.js

---
+++ docs/test-3/level-2/assets/myjs.js
@@ -0,0 +1,6 @@
+    var button = document.querySelector( "button" );
+ 
+    // If a user clicks on it, say hello!
+    button.addEventListener( "click", function( ev ) {
+        alert( "Hello" );
+    }, false)
